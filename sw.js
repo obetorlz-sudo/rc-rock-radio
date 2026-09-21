0940089257e9c36185cfa17cfa850760e7d46c27
obetorlz-sudo <obetorlz@gmail.com>
2026-09-21 11:10:15 -0300

    V7: bust PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rc-rock-radio-v6-dog-map-fastmeta';
+const CACHE='rc-rock-radio-v7-rockdog-map';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
