a3e50f0f2339825888ccc594f6eb086899e21dde
obetorlz-sudo <obetorlz@gmail.com>
2026-09-21 11:56:15 -0300

    V10: refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='rc-rock-radio-v9-real-rockdog-logo';
-const ASSETS=['./','./index.html','./styles.css?v=9.0','./app.js?v=9.0','./manifest.webmanifest?v=9.0','./icon.svg?v=9.0','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
+const CACHE='rc-rock-radio-v10-live-now-playing';
+const ASSETS=['./','./index.html','./styles.css?v=10.0','./app.js?v=10.0','./manifest.webmanifest?v=10.0','./icon.svg?v=10.0','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
